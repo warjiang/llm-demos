a85a21c950968a93ed20a5bf84ebc419083d2e03
warjiang <1096409085@qq.com>
2025-05-25 17:25:29 +0800

    feat: add mcp client
    Signed-off-by: warjiang <1096409085@qq.com>

---
+++ mcp/main.py
@@ -0,0 +1,35 @@
+from dotenv import load_dotenv
+from mymcpclient import MyMCPClient
+
+import os
+import asyncio
+
+
+load_dotenv()
+
+
+async def main():
+    print(os.environ.get("API_KEY"))
+    my_mcp_client = MyMCPClient(
+        base_url=os.environ.get("BASE_URL"),
+        api_key=os.environ.get("API_KEY"),
+        model=os.environ.get("MODEL")
+    )
+    try:
+        await my_mcp_client.connect_to_stdio_server(
+            'karmada-mcp-server',
+            '/Users/dingwenjiang/workspace/codereview/karmada-mcp-server/_output/bin/darwin/arm64/karmada-mcp-server',
+            [
+                "stdio",
+                "--karmada-kubeconfig=/Users/dingwenjiang/.kube/karmada.config",
+                "--karmada-context=karmada-apiserver",
+                "--skip-karmada-apiserver-tls-verify"
+            ]
+        )
+        await my_mcp_client.chat_loop()
+    finally:
+        await my_mcp_client.cleanup()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
